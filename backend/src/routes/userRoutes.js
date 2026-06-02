7a5a36cc0b75387f061cb87006ed643f846cdbdc
Chris <loudowl@mac.com>
2026-06-01 17:08:17 -0700

    feat: initial implementation

---
+++ backend/src/routes/userRoutes.js
@@ -0,0 +1,11 @@
+// User management routes
+
+const express = require('express');
+const { register, login } = require('../controllers/userController');
+
+const router = express.Router();
+
+router.post('/register', register);
+router.post('/login', login);
+
+module.exports = router;
