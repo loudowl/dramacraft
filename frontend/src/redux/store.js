0b8d4ac47c0501afd4c02b9556809616b166698e
Chris <loudowl@mac.com>
2026-06-01 17:08:39 -0700

    feat: initial implementation

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,10 @@
+// Redux store configuration
+
+import { createStore, combineReducers } from 'redux';
+import userReducer from './reducers/userReducer';
+
+const rootReducer = combineReducers({
+  user: userReducer,
+});
+
+export const store = createStore(rootReducer);
