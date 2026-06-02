57176aaf4d079e60ac659ce7791a7737822bf281
Chris <loudowl@mac.com>
2026-06-01 17:08:30 -0700

    feat: initial implementation

---
+++ backend/src/utils/database.js
@@ -0,0 +1,9 @@
+// Database utility
+
+const { Sequelize } = require('sequelize');
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+});
+
+module.exports = sequelize;
